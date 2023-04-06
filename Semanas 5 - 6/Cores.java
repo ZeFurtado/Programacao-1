17e697f658a6aa0528c08e7024cb7b2b4c6aeb15
Lukhas Furtado <lukhasfurtado9@gmail.com>
2023-04-06 03:03:04 -0300

    Testando o intellij

---
+++ Semanas 5 - 6/Cores.java
@@ -0,0 +1,12 @@
+public class Cores {
+    public int R;
+    public int G;
+    public int B;
+
+    public Cores (int r, int g, int b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+}
