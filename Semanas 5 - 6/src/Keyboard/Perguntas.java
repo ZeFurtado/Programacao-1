d26a44a9a697ab3df6273267e7cb51ac8ea69d21
Lukhas Furtado <lukhasfurtado9@gmail.com>
2023-04-22 05:57:27 -0300

    Semana 5-6 concluída

--- Semanas 5 - 6/src/Keyboard/Perguntas.java
+++ Semanas 5 - 6/src/Keyboard/Perguntas.java
@@ -3,7 +3,9 @@ package Keyboard;
 public class Perguntas {
     public static void main(String[] args) {
         Teclado teclado = new Teclado();
-        int num = teclado.leInt("DIgite um número inteiro");
-        System.out.println("O número que você digitou foi " + num);
+
+
+
+
     }
 }
