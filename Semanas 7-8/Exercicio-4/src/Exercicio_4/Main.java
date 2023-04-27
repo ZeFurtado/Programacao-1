cb4f595e8e1a48f888cb0479c74a614785706674
Lukhas Furtado <lukhasfurtado9@gmail.com>
2023-04-27 17:11:07 -0300

    Início do Desafio de Programação-1

--- Semanas 7-8/Exercicio-4/src/Exercicio_4/Main.java
+++ Semanas 7-8/Exercicio-4/src/Exercicio_4/Main.java
@@ -2,14 +2,19 @@ package Exercicio_4;
 
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+
         Carro carroAjenor = new Carro("Honda", 50000);
         carroAjenor.setPreco(90.000);
-
-
         Cliente cliente = new Cliente("Ajenor", carroAjenor);
 
 
-
+        System.out.println(main.devolveValor('a'));
         System.out.println("O carro do pinta é: "+carroAjenor.getMarca()+ " e custa R$:"+carroAjenor.getPreco());
+    }
+
+
+    public int devolveValor(char letra){
+        return letra;
     }
 }
